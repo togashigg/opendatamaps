53fe828bd04f792d2fcf2d3139c5fb058908e6e3
TOGASHI Nobukazu <togashigg@gmail.com>
2023-06-05 14:07:25 +0900

    update

--- djangoapp/static/js/opendatamaps.js
+++ djangoapp/static/js/opendatamaps.js
@@ -324,7 +324,6 @@
 	// 種別選択（検索対象）
 	function changekindListSelect(select) {
 		if(DEBUG) console.log(nowToString()+' changekindListSelect() start');
-		alert('changekindListSelect start');
 		let opts=select.getElementsByTagName('option');
 		for(let i=0; i<select.childElementCount; i++) {
 			let kind=opts[i].value;
@@ -349,7 +348,6 @@
 	function clickkindListOption(opt) {
 /* ToDo:
 		if(DEBUG) console.log(nowToString()+' clickkindListOption() start');
-		alert('clickkindListOption start');
 		let kind=opt.value;
 		let text=opt.text;
 		if(text.substring(0,1)=='☑') {
